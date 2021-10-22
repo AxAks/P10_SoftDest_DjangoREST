0ad04e313f504851cc0a851979286a6c6b1374de
ax <axel.akonde@cinego.net>
2021-10-22 15:31:20 +0200

    creation model CustomUser + creation apps : Core et Project + edit setting.py

--- SoftDesk/SoftDesk/settings.py
+++ SoftDesk/SoftDesk/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework'
+    'core'
+    'project'
 ]
 
 REST_FRAMEWORK = {
@@ -89,6 +91,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'core.CustomUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -112,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr'
 
 TIME_ZONE = 'UTC'
 
---
+++ core/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    """
+    Extends the Basic User class without adding anything to it
+    in order to be able to override it easily in the future, if needed
+    """
+    pass
