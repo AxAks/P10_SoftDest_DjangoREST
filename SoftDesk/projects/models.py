15c6752aaf9002ebb03562d8124a944aabcd41b8
Aksel <axel.akonde@gmail.com>
2021-10-30 01:58:25 +0200

    ajout/corrections models projects et reprise de la DB

--- SoftDesk/projects/models.py
+++ SoftDesk/projects/models.py
@@ -10,19 +10,19 @@ class Project(models.Model):
     title = models.CharField(max_length=128)
     description = models.CharField(max_length=1024)
     type = models.CharField(max_length=128)
-    author_user = models.ForeignKey(
+    author = models.ForeignKey(
         to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'"{self.title}" by {self.author_user}: {self.type}'
+        return f'"{self.title}" by {self.author}: {self.type}'
 
     objects = models.Manager()
 
 
 class Contributor(models.Model):
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    project_id = models.ForeignKey(to=Project, on_delete=models.CASCADE)
+    project = models.ForeignKey(to=Project, on_delete=models.CASCADE)
     permission = models.Choices('read', 'write')  # choix à remplir
     role = models.CharField(max_length=128)
 
@@ -36,10 +36,10 @@ class Issue(models.Model):
     priority = models.CharField(max_length=20)
     project = models.ForeignKey(to=Project, on_delete=models.CASCADE)
     status = models.CharField(max_length=20)
-    author_user = models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='author_user_id',
+    author = models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='author',
                                     on_delete=models.CASCADE)
-    assignee_user = models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='assignee_user_id',
-                                        on_delete=models.CASCADE)
+    assignee = models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='assignee',
+                                      on_delete=models.CASCADE)
     created_time = models.DateTimeField(auto_now_add=True)
 
     objects = models.Manager()
@@ -47,7 +47,7 @@ class Issue(models.Model):
 
 class Comment(models.Model):
     description = models.CharField(max_length=1024)
-    author_user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     issue = models.ForeignKey(to=Issue, on_delete=models.CASCADE)
     created_time = models.DateTimeField(auto_now_add=True)
 
