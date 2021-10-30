2d628552b6434f0b992958565edc650663be39c7
Aksel <axel.akonde@gmail.com>
2021-10-30 02:11:39 +0200

    serializers : comment et issue

--- SoftDesk/projects/serializers.py
+++ SoftDesk/projects/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from projects.models import Project
+from projects.models import Project, Issue, Comment
 
 
 class ProjectSerializer(serializers.ModelSerializer):
@@ -8,3 +8,17 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = ['title', 'description', 'type', 'author']
+
+
+class IssueSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Issue
+        fields = ['title', 'description', 'tag', 'priority', 'project', 'status', 'author', 'assignee']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = ['description', 'author', 'issue']
