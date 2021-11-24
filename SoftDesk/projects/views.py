8f67833e6bf571f5c7d443dd8c70b622f9119434
ax <axel.akonde@cinego.net>
2021-11-24 17:28:40 +0100

    desactivation temporaire  de la verif manager dans update contrib

--- SoftDesk/projects/views.py
+++ SoftDesk/projects/views.py
@@ -133,11 +133,14 @@ class ContributorModelViewSet(ModelViewSet):
         contributor = lib_projects.find_contributor(self.queryset, kwargs)
         self.check_object_permissions(request, contributor)
         contributor.role = request.data['role'] if 'role' in request.data.keys() else contributor.role
+        contributor.save()
+        """
         contrib_as_dict = model_to_dict(contributor) # marche pas car le user est deja dans le projet en tant que contrib
         serializer = self.serializer_class(data=contrib_as_dict)
         serializer.is_valid(raise_exception=True)
         serializer.save(contributor.project)
-        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+        """
+        return Response(contributor.data, status=status.HTTP_204_NO_CONTENT)
 
     def destroy(self, request, **kwargs):
         """
