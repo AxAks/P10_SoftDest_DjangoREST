797b8a867fed9635581b7237494681b15f7d2018
ax <axel.akonde@cinego.net>
2021-12-10 18:02:16 +0100

    Ajout Endpoint token_refresh + expiration du token à 10 minutes

--- SoftDesk/users/urls.py
+++ SoftDesk/users/urls.py
@@ -1,5 +1,7 @@
+from django.conf.urls import url
 from django.urls import path
 from users.views import CreateUserModelViewSet, AuthenticationAPIView, PersonalInfosModelViewSet
+from rest_framework_jwt.views import refresh_jwt_token
 
 app_name = "users"
 
@@ -8,6 +10,7 @@ urlpatterns = [
         'post': 'create'
     })),
     path('login/', AuthenticationAPIView.as_view()),
+    url('token_refresh/', refresh_jwt_token),
     path('my_infos/', PersonalInfosModelViewSet.as_view(
         {
             'get': 'retrieve',
